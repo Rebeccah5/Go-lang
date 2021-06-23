d9c86f1c208c364a15e1a824e8ca5af80f3fb200
Rebeccah <rebeccah.wambui@twiga.com>
2021-06-24 01:01:43 +0300

    Add variables and data types

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
+	var number uint16 = 250
+	var language string = "Go-lang"
 	fmt.Println("Hello Becky,welcome to Go-lang!")
+	fmt.Println(number)
+	fmt.Println(language)
 }
